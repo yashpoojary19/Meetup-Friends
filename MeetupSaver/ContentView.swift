7b9c80101f9001c356cba440e0fca2a07f4131a9
Yash Poojary <yashspoojary245@gmail.com>
2021-11-30 13:13:35 +0530

    Initial Commit

---
+++ MeetupSaver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeetupSaver
+//
+//  Created by Yash Poojary on 30/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MeetupSaver/MeetupSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetupSaverApp.swift
+//  MeetupSaver
+//
+//  Created by Yash Poojary on 30/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MeetupSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
